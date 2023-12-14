0b78baf31d78fbf22d43d8dbcf479bb134f6581d
Supakarn Jariyayong <sup.jariyayong@hotmail.com>
2023-12-15 01:29:06 +0800

    fix: fix bug

--- app/application/services/list_attractions.rb
+++ app/application/services/list_attractions.rb
@@ -12,22 +12,30 @@ module TravelRoute
       step :reify_attractions
 
       def lookup_call(input)
-        place_ids = input[:cart]
-        place_ids.map do |place_id|
+        place_ids = input[:cart].keys
+        response_list = place_ids.map do |place_id|
           result = Gateway::Api.new(TravelRoute::App.config).add_attraction(place_id)
           result.success? ? Success(result.payload) : Failure(result.message)
         end
+        unpack(response_list)
       rescue StandardError
         Failure('Could not connect to Travel Route API or place_id doesnt exist')
       end
 
       def reify_attractions(results)
         attractions = results.map do |attraction_json|
-          Representer::Attractions.new(OpenStruct.new).from_json(attraction_json)
+          Representer::Attraction.new(OpenStruct.new).from_json(attraction_json)
         end
         Success(attractions)
       rescue StandardError
         Failure('Error in parsing Attraction')
+      end
+
+      # helper methods
+      def unpack(list_of_response)
+        return Failure(list_of_response.map(&:failure?).map(&:value!)) unless list_of_response.map(&:success?).all?
+
+        Success(list_of_response.map(&:value!))
       end
     end
   end
