464570e67c5b44ba699974a62b50a06ae46d016a
Supakarn Jariyayong <sup.jariyayong@hotmail.com>
2023-12-16 01:40:03 +0800

    feat: map pin convertion

--- app/presentation/view_objects/plan.rb
+++ app/presentation/view_objects/plan.rb
@@ -5,25 +5,37 @@ require_relative 'route_attraction'
 module Views
   # View object for plan
   class Plan
-    attr_reader :plan, :name
+    attr_reader :name
 
     def initialize(plan, name = 'Untitled')
       @plan = plan
       @name = name
     end
 
+    def plan
+      @plan.value!
+    end
+
     def all_attractions
-      @plan.attractions
+      plan.attractions
     end
 
     def origin
-      @plan.attractions.first
+      plan.attractions.first
+    end
+
+    def last
+      plan.attractions.last
     end
 
     def route_to_attractions
-      @plan.routes.zip(@plan.attractions[1..]).map do |route, attraction|
+      plan.routes.zip(plan.attractions[1..]).map do |route, attraction|
         RouteAttraction.new(route, attraction)
       end
+    end
+
+    def to_pin
+      AttractionList.new(all_attractions).to_map_pins
     end
   end
 end
